5b423301ab0c095a398ca0e2383396dea33ecf9f
PeraltaRomeroFacundoRodolfo <facundoperalta217@gmail.com>
2025-09-05 20:20:48 -0300

    2da parte hecha

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sad2.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+
+ [Get] GetPedidos() => Retorna una lista de Pedidos
+ [Get] GetCadetes() => Retorna una lista de Cadetes
+ [Get] GetInforme() => Retorna un objeto Informe
+ [Post] AgregarPedido(Pedido pedido)
+ [Put] AsignarPedido(int idPedido, int idCadete)
+[Put] CambiarEstadoPedido(int idPedido,int NuevoEstado)
+ [Put] CambiarCadetePedido(int idPedido,int idNuevoCadete)
+}
